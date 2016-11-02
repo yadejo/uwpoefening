48ca387c523c3bef12b5b7d369de8d6e03d6eaee
yadejo <yannickdejonghe@gmail.com>
2016-11-02 11:09:42 +0100

    Changed Startup View to MainPage

--- TrafficLights/TrafficLights.UI/App.xaml.cs
+++ TrafficLights/TrafficLights.UI/App.xaml.cs
@@ -72,7 +72,7 @@ namespace TrafficLights.UI
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(View.TrafficLightCreatePage), e.Arguments);
+                    rootFrame.Navigate(typeof(View.MainPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
